7bdad739e9b77d2a79c36aeda55e5a27645765b0
gyuzero <gyuzero0103@gmail.com>
2023-08-06 19:10:14 +0900

    initial commit

---
+++ src/main/java/com/example/springhessianconfig/SpringHessianConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhessianconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHessianConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHessianConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springhessianconfig/SpringHessianConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhessianconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHessianConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
